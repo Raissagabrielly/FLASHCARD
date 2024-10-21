ab58c612c31921393c9cf124f901c15d78d68f38
Raissagabrielly <160615769+Raissagabrielly@users.noreply.github.com>
2024-10-21 12:21:18 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Algoritmo?',
+    'O Algoritmo é uma sequência ordenada,definida e finita de ações que visam a solução de um determinado problema computacional'
+)
+
+criaCartao(
+    'Português',
+    'Alentar é o mesmo que?',
+    'Alentar é o mesmo que animar,encorajar'
+)
+
+criaCartao(
+    'Programação',
+    'Por que é necessário realizar testes de software?',
+    'È uma forma de identificar e solucionar os erros de um sistema antes que ela chegue até o cliente'
+)
+
+criaCartao(
+    'História',
+    'De quem é a famosa frase "penso,logo existo?',
+    'Descartes'
+)
